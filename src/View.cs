07149a65667120e07fb5322c1a1fefccffcc02b3
yoshimi.suzuki <ys443ys443@yahoo.co.jp>
2018-02-12 01:39:59 +0900

    remove event when detached

--- src/View.cs
+++ src/View.cs
@@ -28,6 +28,8 @@ namespace UnityMVC
             if (_isDetached)
                 return;
 
+            _model.OnPositionUpdated -= UpdatePosition;
+
             GameObject.Destroy(GetRoot());
             _isDetached = true;
         }
